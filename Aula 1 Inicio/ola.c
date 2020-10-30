9f1eb148643d77a15fa6d148c59d072013c61128
JABMacedo <73313083+JABMacedo@users.noreply.github.com>
2020-10-30 19:41:30 +0000

    1 commit

---
+++ Aula 1 Inicio/ola.c
@@ -0,0 +1,21 @@
+/*
+autor : Jose Macedo
+email:
+date:
+descricao: ...identificacao sobre o que faz...
+
+observacoes
+
+*/
+
+#include <stdio.h>
+
+/*
+mostra o que está dentro do printf
+\n - muda de linha "new line"
+*/
+int main(){
+    printf("viva o...\n")
+}
+
+
---
+++ Exercicios/aulac/aula.c
@@ -0,0 +1,35 @@
+/**
+ * @file aula.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2020-10-27
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+
+#include <stdio.h>
+
+//titulo
+/*
+documentar
+*/
+int main ()
+{
+    int x = 1;
+    printf("%d\n", x);
+}
+
+/**
+ * @brief 
+ * 
+ * @param x 
+ * @param y 
+ * @return int 
+ */
+int soma (int x, int y)
+{
+    
+}
